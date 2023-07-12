38f03493e60d7fc52cab90f584cc2de24c8d5a90
Antônio Pires <antoniovpiresnt@gmail.com>
2023-07-12 16:16:57 -0300

    add new aesthetics

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -61,11 +61,13 @@ class App extends React.Component {
           <p>No último ano e meio tenho me dedicado ao estudo da programação, aprimorando constantemente minhas habilidades e ampliando meu conhecimento. Durante esse período, tive a oportunidade de trabalhar como desenvolvedor web em tempo integral por 11 meses, adquirindo uma valiosa experiência prática na área. Além das minhas atividades profissionais, participei ativamente de projetos paralelos, o que enriqueceu ainda mais minha expertise e alimentou minha paixão por criar soluções inovadoras.</p>` : `<h2>My experience</h2>
           <p>I have dedicated the past year and a half to immersing myself in the world of programming, constantly honing my skills and expanding my knowledge. During this time, I have had the privilege of working as a full-time web developer for 11 months, gaining valuable hands-on experience in the industry. In addition to my professional endeavors, I have actively participated in various side projects, which have further enriched my expertise and fueled my passion for creating innovative solutions.</p>`;
 
-        const projectDescriptionText1 = language === "br" ? "Tenho experiência em eCommerce e projetos relacionados. Desenvolvi um sistema CRUD para cadastro, atualização e exclusão de produtos em uma loja fictícia, utilizando NoSQL e o ORM Prisma." : "I have experience in eCommerce and have worked on related projects. In one project, I developed a CRUD system for registering, updating, and deleting products in a fictional store, using NoSQL with the Prisma ORM.";
-        const projectTitleText1 = language === "br" ? "Cadastro de Produtos para eCommerce" : "eCommerce Product Register";
 
-        const projectDescriptionText2 = language === "br" ? "Esse projeto tinha como objetivo criar um blog simples e objetivo, inspirado em um caderno de anotações, facilitando o registro e a edição dos conteúdos conforme necessário." : "This project aimed to create a simple and concise blog inspired by a notebook, allowing easy record-keeping and effortless editing when needed.";
-        const projectTitleText2 = language === "br" ? "Blog no estilo Diário de bolso" : "Diary like blog";
+        const projectTitleText1 = "EZ Youtube Video Downloader";
+        const projectDescriptionText1 = language === "br" ? "Criei esse aplicativo desktop com o objetivo de ter um jeito fácil e rápido de baixar conteúdo do Youtube. Utilizei Python, PyTube e Tkinter para o app, para transformá-lo em um executável, utilizei pyinstaller." : "I have made this desktop app with the objetctive of having a fast and easy way to download youtube content. I used Python, PyTube and Tkinter for the app, and to transform it into an executable I used pyinstaller";
+        
+        const projectTitleText2 = language === "br" ? "Página para empresa fictícia" : "Made-up Company Web Page";
+        const projectDescriptionText2 = language === "br" ? "Esse é um dos projetos que possuo para páginas webs, servem para praticar código e noções de UX e UI. Me divirto bastante com esse tipo de site, consigo treinar várias habilidades." : "This is one of the projects I have for web pages, they are good to practice coding and bot UX and UI notions. I have a lot of fun with this kind of website, since I can practice different skills";
+        
 
         const aboutMeTitle = language === "br" ? "Sobre mim" : "About me";
         const aboutMeText = language === "br" ? "Tenho a ambição de impactar o mundo positivamente através da tecnologia. Gosto de trabalhar em projetos inovadores e adoraria fazer parte de trabalhos voluntários que envolvam programação. Me dou muito bem com trabalho em equipe e tenho facilidade para me comunicar com pessoas (incluindo públicos maiores), tanto em Português quanto em Inglês. Sou autodidata, sei me autogerenciar, estou sempre atrás de novos conhecimentos e buscando aprimorar as minhas habilidades." : "I have the ambition to positively impact the world through technology. I enjoy working on innovative projects and would love to be part of volunteer work involving programming. I excel in teamwork and have a knack for effectively communicating with people, including larger audiences, both in Portuguese and English. I am self-taught, capable of self-management, and always seeking new knowledge to improve my skills."
@@ -95,7 +97,7 @@ class App extends React.Component {
         return (
             <div className="App">
                 <aside>
-                    <img id="profilePicture" src={require('./project-pictures/canvas.png')} alt="Man detailed in pixels"></img>
+                    <img id="profilePicture" src={require('./project-pictures/foto_perfil.png')} alt="Man detailed in pixels"></img>
                     <h3 id="profileTitle">Antônio V. Pires</h3>
                     <p id="profileSubtitle">Web Developer</p>
                     <p id="profileDescription">
@@ -121,11 +123,13 @@ class App extends React.Component {
                         <small id="swiperTitle"></small>
                         <Swiper language={language} />
                     </section>
-                    <section id="projeto1" className="projectDiv project2 fade-right">
+                    <section id="projeto1" className="projectDiv project1 fade-right">
                         <ProjectDescription title={projectTitleText1} text={projectDescriptionText1} />
                     </section>
-                    <section className="projectDiv project1 fade-left">
-                        <ProjectDescription title={projectTitleText2} text={projectDescriptionText2} />
+                    <section id="projeto2" className="projectDiv project2 fade-left">
+                        <a href="https://antoniovpires.github.io/insta_help/" target="_blank">
+                            <ProjectDescription title={projectTitleText2} text={projectDescriptionText2} />
+                        </a>
                     </section>
                     <TextDiv title={aboutMeTitle} text={aboutMeText} />
                     <section className="aboutDiv fade-right">
@@ -135,8 +139,8 @@ class App extends React.Component {
                             <SkillDiv title="Frameworks/Libraries" icons={[{ class: 'devicon-react-original colored', name: 'ReactJS' }, { class: 'devicon-jquery-plain-wordmark colored', name: 'jQuery' }, { class: 'devicon-express-original colored', name: 'ExpressJS' }, { class: 'devicon-bootstrap-plain colored', name: 'Bootstrap' }]} />
                             <SkillDiv title="Back-End" icons={[{ class: 'devicon-nodejs-plain colored', name: 'NodeJs' }, { class: 'devicon-python-plain colored', name: 'Python' }, { class: 'devicon-php-plain colored', name: 'PHP' }]} />
                             <SkillDiv title="Databases" icons={[{ class: 'devicon-mysql-plain-wordmark colored', name: 'MySql' }]} />
-                            <SkillDiv title="Tools" icons={[{ class: 'devicon-linux-plain colored', name: 'Linux' }, { class: 'devicon-git-plain colored', name: 'Git' }, { class: 'devicon-github-original colored', name: 'GitHub' }, { class: 'devicon-jira-plain colored', name: 'Jira' }]} />
-                            <SkillDiv title="In Progress" icons={[{ class: 'devicon-debian-plain colored', name: 'Debian' }, { class: 'devicon-docker-plain colored', name: 'Docker' }, { class: 'devicon-apache-line-wordmark colored', name: 'Apache' }, { class: 'devicon-amazonwebservices-plain-wordmark colored', name: 'AWS' }]} />
+                            <SkillDiv title="Tools" icons={[{ class: 'devicon-linux-plain colored', name: 'Linux' }, { class: 'devicon-git-plain colored', name: 'Git' }, { class: 'devicon-github-original colored', name: 'GitHub' }, { class: 'devicon-jira-plain colored', name: 'Jira' }, { class: 'devicon-debian-plain colored', name: 'Debian' }, { class: 'devicon-docker-plain colored', name: 'Docker' }, { class: 'devicon-apache-line-wordmark colored', name: 'Apache' }, { class: 'devicon-amazonwebservices-plain-wordmark colored', name: 'AWS' }]} />
+                            {/* <SkillDiv title="In Progress" icons={[]} /> */}
                         </div>
                     </section>
                     <footer>
